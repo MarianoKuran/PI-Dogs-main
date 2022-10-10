f410811428a3df3004fcee349f96bc0e7092312e
Mariano Kuran <marianokuran6@gmail.com>
2022-10-10 14:07:54 -0300

    change sequelize conection url

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, PGDATABASE, PGHOST, PGPORT
+  DB_USER, DB_PASSWORD, DB_HOST, PGDATABASE, PGHOST, PGPORT, PGUSER
 } = process.env;
 
 const sequelize = new Sequelize(`postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}`, {
